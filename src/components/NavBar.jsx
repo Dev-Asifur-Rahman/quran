34f0f6ca97a348adf4bb2654c4c52c9fe3252a01
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-03-24 23:50:00 +0600

    input search ui is finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -70,28 +70,43 @@ const NavBar = () => {
             />
           </div>
         </div>
-        <div className="navbar-center">
-          <div>
-            <label className="input">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+        <div className="navbar-center flex w-[45%]  justify-center relative">
+          <label className="input w-full">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
               >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <path d="m21 21-4.3-4.3"></path>
-                </g>
-              </svg>
-              <input type="search" required placeholder="Search" />
-            </label>
-          </div>
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              type="search"
+              className=""
+              required
+              placeholder="Search Ayah"
+            />
+          </label>
+          <ul className="menu bg-base-200 rounded-box w-full absolute top-full mt-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+
           {/* <ul className="menu menu-horizontal px-1">
             <li>
               <a>Item 1</a>
