9e60e319aae7bec771b9a46f848f90e944e20f08
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-17 09:14:54 +0600

    searchbar wrapped into searchbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import Sidebar from "./Sidebar";
 import Image from "next/image";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -70,65 +71,7 @@ const NavBar = () => {
             />
           </div>
         </div>
-        <div className="navbar-center flex w-[45%]  justify-center relative">
-          <label className="input w-full">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-              </g>
-            </svg>
-            <input
-              type="search"
-              className=""
-              required
-              placeholder="Search Ayah"
-            />
-          </label>
-          <ul className="menu bg-base-200 rounded-box w-full absolute top-full mt-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 2</a>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-
-          {/* <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul> */}
-        </div>
+        <SearchBar></SearchBar>
         <div className="navbar-end">
           <div>
             <Sidebar></Sidebar>
